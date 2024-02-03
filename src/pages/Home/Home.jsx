6ebd7315f5b077282e09d67d155dce5a01f2dcee
Tanveer Hossain Jony <thjbd19@gmail.com>
2024-02-03 21:58:02 +0600

    update portfolios

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Home.css";
 
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
@@ -27,46 +28,133 @@ const Home = () => {
           </h2>
         </div>
 
-        <Card sx={{ maxWidth: 345 }}>
-          <CardMedia
-            component="img"
-            alt="green iguana"
-            height="140"
-            image="/img/a10-512.jpg"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Lizard
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Lizards are a widespread group of squamate reptiles, with over
-              6,000 species, ranging across all continents except Antarctica
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small">
-              <a
-                target="_blank"
-                href="https://github.com/tanveer19/A12-karate-camp-client"
-              >
-                Client
-              </a>
-            </Button>
-            <Button size="small">
-              <a
-                target="_blank"
-                href="https://github.com/tanveer19/A12-karate-camp-server"
-              >
-                Server
-              </a>
-            </Button>
-            <Button size="small">
-              <a target="_blank" href="https://a12-summer-camp-1ce70.web.app/">
-                Live Site
-              </a>
-            </Button>
-          </CardActions>
-        </Card>
+        <div className="flex justify-center gap-5">
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+              component="img"
+              alt="green iguana"
+              height="140"
+              image="/img/a12-512.jpg"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Karate Camp
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Learn karate from famous instructors!
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">
+                <a
+                  target="_blank"
+                  href="https://github.com/tanveer19/A12-karate-camp-client"
+                >
+                  Client
+                </a>
+              </Button>
+              <Button size="small">
+                <a
+                  target="_blank"
+                  href="https://github.com/tanveer19/A12-karate-camp-server"
+                >
+                  Server
+                </a>
+              </Button>
+              <Button size="small">
+                <a
+                  target="_blank"
+                  href="https://a12-summer-camp-1ce70.web.app/"
+                >
+                  Live Site
+                </a>
+              </Button>
+            </CardActions>
+          </Card>
+
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+              component="img"
+              alt="green iguana"
+              height="140"
+              image="/img/a11-512.jpg"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Toy Story
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                A toy store for your favorite toys!
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">
+                <a
+                  target="_blank"
+                  href="https://github.com/tanveer19/A11-toy-story-client"
+                >
+                  Client
+                </a>
+              </Button>
+              <Button size="small">
+                <a
+                  target="_blank"
+                  href="https://github.com/tanveer19/A11-toy-story-server"
+                >
+                  Server
+                </a>
+              </Button>
+              <Button size="small">
+                <a target="_blank" href="https://a11-toy.web.app/">
+                  Live Site
+                </a>
+              </Button>
+            </CardActions>
+          </Card>
+
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+              component="img"
+              alt="green iguana"
+              height="140"
+              image="/img/a10-512.jpg"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Cafe Onion
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Your delicious recipes!
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">
+                <a
+                  target="_blank"
+                  href="https://github.com/tanveer19/A10-cafe-onion-client"
+                >
+                  Client
+                </a>
+              </Button>
+              <Button size="small">
+                <a
+                  target="_blank"
+                  href="https://github.com/tanveer19/A10-cafe-onion-server"
+                >
+                  Server
+                </a>
+              </Button>
+              <Button size="small">
+                <a
+                  target="_blank"
+                  href="https://a10-chef-recipe-hunter-8182f.web.app"
+                >
+                  Live Site
+                </a>
+              </Button>
+            </CardActions>
+          </Card>
+        </div>
 
         <div className="icons">
           <a href="https://github.com/tanveer19" target="_blank">
